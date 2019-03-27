f3de184d261b9f9a15755b1824a15e0b2f82606c
YOGESH GOEL <yogesh2699@gmail.com>
2019-03-27 11:29:22 +0530

    Add files via upload

--- matrix.java
+++ matrix.java
@@ -56,14 +56,14 @@ public class matrix {
           //Append a row Array with Temporary Matrix
           for(int i=0;i<N;i++){
         	  int element = row[i];
-        	  temparr[i][N] = element;
+        	  temparr[i][M] = element;
           }
           //smallest element in Matrix Column are inserted in Array col
           int[] col = smallestInCol(N,M+1,temparr,col_result);
           //Append col Array with temporary Matrix.
-          for(int i=0;i<=M;i++){
+        for(int i=0;i<M+1;i++){
         	  int element = col[i];
-        	  temparr[M][i] = element;
+        	  temparr[N][i] = element;
           }
           
           //Printing the Newly Created Matrix
@@ -96,7 +96,7 @@ public class matrix {
 		 int[] col_result = new int[M+1];
 		 // Calling the function  
 		 //largest element in Matrix rows are insert in Array row
-                int[] row =  largestInRow(N, arr, row_result); 
+                int[] row =  largestInRow(N, M, arr, row_result); 
               //Create a Temporary array to append Sorted Array row with Matrix.
           int[][] temparr = new int[N+1][M+1];
           for(int i=0;i<N+1;i++){
@@ -112,14 +112,14 @@ public class matrix {
           //Append a row Array with Temporary Matrix
           for(int i=0;i<N;i++){
         	  int element = row[i];
-        	  temparr[i][N] = element;
+        	  temparr[i][M] = element;
           }
           //largest element in Matrix Column are inserted in Array col
-          int[] col = largestInCol(M+1,temparr,col_result);
+          int[] col = largestInCol(N,M+1,temparr,col_result);
         //Append a col Array with Temporary Matrix
-          for(int i=0;i<=M;i++){
+          for(int i=0;i<M+1;i++){
         	  int element = col[i];
-        	  temparr[M][i] = element;
+        	  temparr[N][i] = element;
           }
           System.out.println("Newly Created Matrix in Maximum operation:-");
           for(int i=0;i<N+1;i++){
@@ -129,29 +129,7 @@ public class matrix {
         	  System.out.println(" ");
         	  }
 	}
-	// Function to get max element in Row
-    public static int[] largestInRow(int no_of_rows, int[][] arr, int[] row_result) { 
-        int i = 0; 
-          
-        // Initialize max to 0 at beginning
-        // of finding max element of each row 
-        int max = 0; 
-         
-        while (i < no_of_rows) { 
-            for (int j = 0; j < arr[i].length; j++) { 
-                if (arr[i][j] > max) { 
-                    max = arr[i][j]; 
-                } 
-            } 
-            row_result[i] = max; 
-            max =0; 
-            i++; 
-          
-        } 
-        
-		return row_result; 
-  
-    } 
+	 
  // Function to get minimum element in column
     public static int[] smallestInCol(int n,int m , int[][] temparr, int[] col_result) { 
     	for (int i = 0; i < m; i++) { 
@@ -204,26 +182,54 @@ public class matrix {
   
     }
  // Function to get maximum element in column
-    public static int[] largestInCol(int cols, int[][] temparr, int[] col_result) { 
-        int i = 0; 
-          
-        // Initialize max to 0 at beginning 
-        // of finding max element of each row 
-        int max = 0; 
-         
-        while (i < cols) { 
-            for (int j = 1; j < temparr[i].length; j++) { 
-                if (temparr[j][i] > max) { 
-                    max = temparr[j][i]; 
+    public static int[] largestInCol(int n,int m , int[][] temparr, int[] col_result) { 
+    	for (int i = 0; i < m; i++) { 
+  		  
+            // initialize the maximum element  
+            // as first element  
+            int minm = temparr[0][i]; 
+  
+            // Run the inner loop for columns  
+            for (int j = 1; j < n; j++) { 
+  
+                // check if any element is smaller  
+                // than the maximum element of the column  
+                // and replace it  
+                if (temparr[j][i] > minm) { 
+                    minm = temparr[j][i]; 
                 } 
             } 
-            col_result[i] = max; 
-            max =0; 
-            i++; 
-          
+  
+            // insert the largest element of the row in col_result array  
+            col_result[i] = minm; 
         } 
         
 		return col_result; 
+  
+    }
+ // Function to get max element in Row
+    public static int[] largestInRow(int n,int m, int[][] arr, int[] row_result) { 
+    	for (int i = 0; i < n; i++) { 
+  		  
+            // initialize the maximum element  
+            // as first element  
+            int minm = arr[i][0]; 
+  
+            for (int j = 1; j < m; j++) { 
+  
+                // check if any element is smaller  
+                // than the maximum element of the row  
+                // and replace it  
+                if (arr[i][j] > minm) { 
+                    minm = arr[i][j]; 
+                } 
+            } 
+  
+            // insert the largest element of the row in row_result array  
+            row_result[i] = minm; 
+        }
+        
+		return row_result;  
   
     }
     }
